e88bb6797219d741ab4b9f1f98e52a65f9e397c5
akshayjoyinfo <akshayjoyinfo@gmail.com>
2016-12-26 02:34:55 +0530

    committed new methods in tracking-bot
    committed new methods in tracking-bot

--- app.js
+++ app.js
@@ -1,14 +1,29 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var sql = require('mssql');
  
 var app = express();
-var port = process.env.PORT;
+var port = process.env.PORT||1337;
  
 // body parser middleware
 app.use(bodyParser.urlencoded({ extended: true }));
  
 // test route
-app.get('/', function (req, res) { res.status(200).send('Hello world!'); });
+app.get('/', function (req, res) { 
+  res.status(200).send('Hello world!'); 
+});
+
+app.get('/getClickByRef',function (req, res){
+  var clickRefId = req.query.clickId;
+  console.log(clickRefId);
+  // Select query to iterate all the Portal and retrive the exist or not exist
+  var resultId = "xxxx-"+clickRefId; 
+ // res.status(200).send(resultId);
+  var customResponse = { clickId : clickRefId , ClickReferenceId : resultId};
+
+   res.status(200).send(resultId); 
+})
+
  
 app.listen(port, function () {
   console.log('Listening on port ' + port);
